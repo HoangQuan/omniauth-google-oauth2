58fc814c92458342215b02f0ade25a9018e92f58
Quan Hoang Van (framgia) <hoang.van.quan@framgia.com>
2015-08-31 12:16:21 +0700

    use Rails 4.1 + devise + omniauth-google-oauth2

---
+++ app/controllers/pagescontroller.rb
@@ -0,0 +1,5 @@
+class PagesController < ActionController::Base
+  def user_info
+    render :user_info
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthGoogleOauth2::Application.config.secret_token = '15ac7f7a02d97a545967f2c7f11ec0b1ac177bbd5b20ef983f71e139575797dd45bb76a68afebc0bae4e3101ccd0fb6411c9b640c8ce21a59c41368d24fac2f6'
